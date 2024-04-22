6230ba5d1818238d5e1ba836ac1258ce50e0a106
Hunaee <g.i.nicaise@gmail.com>
2024-04-22 20:40:05 +0200

    troiseme envoi

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+if(isset($_GET['page'])){
+    if($_GET['page'] == 'contact'){
+        include('contact.php');
+    }
 
-echo ("Hello World");
+    else if($_GET['page'] == 'catalogue'){
+        include('catalogue.php');
+    }
+}
+else{
+    include('home.php');
+}
 
-echo("Momo tes très gentil");
+
 
 ?>
